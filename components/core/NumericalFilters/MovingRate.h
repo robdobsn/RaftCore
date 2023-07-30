e8bd4be586c33d87018257097133d1e21f9abf8f
robdobsn <rob@dobson.com>
2023-07-30 09:20:00 +0100

    Fixed MovingRate to handle first few samples better

--- components/core/NumericalFilters/MovingRate.h
+++ components/core/NumericalFilters/MovingRate.h
@@ -18,13 +18,14 @@
  *
  * Calculate the rate of a value like bytes per second
  * The rate is calculated over a moving window of N samples
+ * Uses Arduino-style millis() to get the time
  * 
  * Template N is the window size
  * Template input_t is the type of the input value
  * Template sum_t is the type of the sum
  * 
  */
-template <uint8_t N, class input_t = uint32_t, class sum_t = uint64_t>
+template <uint32_t N, class input_t = uint32_t, class sum_t = uint64_t>
 class MovingRate
 {
 public:
@@ -36,39 +37,54 @@ public:
     // sample
     void sample(input_t input) 
     {
-        previousInputs[index] = input;
-        previousTimeMs[index] = millis();
-        if (++index == N)
-            index = 0;
-        if (usedIdxs < N)
-            usedIdxs++;
+        // Store sample
+        _values[_headIdx] = input;
+        _timestampsMs[_headIdx] = millis();
+
+        // Circular buffer so increment head index
+        if (++_headIdx == N)
+            _headIdx = 0;
+
+        // Increment used slot count
+        if (_usedSlots < N)
+            _usedSlots++;
     }
 
     // get rate
     double getRatePerSec()
     {
-        uint32_t lastIdx = (index + N - 1) % N;
-        uint32_t firstIdx = (lastIdx + N + 1 - usedIdxs) % N;
-        uint32_t timeDeltaMs = Raft::timeElapsed(previousTimeMs[lastIdx], previousTimeMs[firstIdx]);
+        // Check empty
+        if (_usedSlots == 0)
+            return 0;
+
+        // Use modulo arithmetic to get the oldest and newest index
+        uint32_t newestIdx = (_headIdx + N - 1) % N;
+        uint32_t oldestIdx = (_usedSlots == N ? _headIdx : 0);
+
+        // Time delta (using Raft::timeElapsed to handle millis() wraparound
+        // This assumes that the real time delta is less than 2^31 ms
+        uint32_t timeDeltaMs = Raft::timeElapsed(_timestampsMs[newestIdx], _timestampsMs[oldestIdx]);
         if (timeDeltaMs == 0)
             return 0;
-        return (previousInputs[lastIdx] - previousInputs[firstIdx]) * 1000.0 / timeDeltaMs;
+
+        // Calculate rate
+        return (_values[newestIdx] - _values[oldestIdx]) * 1000.0 / timeDeltaMs;
     }
 
     void clear()
     {
-        index = 0;
-        for (uint8_t i = 0; i < N; i++)
+        _headIdx = 0;
+        for (uint32_t i = 0; i < N; i++)
         {
-            previousInputs[i] = 0;
-            previousTimeMs[i] = 0;
+            _values[i] = 0;
+            _timestampsMs[i] = 0;
         }
     }
 
 private:
-    uint8_t index = 0;
-    uint8_t usedIdxs = 0;
-    input_t previousInputs[N] = {};
-    unsigned long previousTimeMs[N] = {};
+    uint32_t _headIdx = 0;
+    uint32_t _usedSlots = 0;
+    input_t _values[N] = {};
+    unsigned long _timestampsMs[N] = {};
 };
 
