5a93cf5abcbfe1772ea180c1b1ff43c45a596dce
Rob Dobson <rob@dobson.com>
2024-03-20 09:54:45 -0400

    Another attempt at CMake conditionals

--- scripts/RaftProject.cmake
+++ scripts/RaftProject.cmake
@@ -255,28 +255,14 @@ add_custom_target(AlwaysCopyFSAndWebUI
     COMMAND echo "-------------- Always copy FS and WebUI files ----------------------"
 )
 
-# Setup command to handle copying of FS and WebUI files
-set(copy_command "
-    ${CMAKE_COMMAND} -E remove_directory \"${_full_fs_dest_image_path}\" &&
-    ${CMAKE_COMMAND} -E copy_directory \"${_full_fs_source_image_path}\" \"${_full_fs_dest_image_path}\" &&
-    ${CMAKE_COMMAND} -E remove \"${_full_fs_dest_image_path}/placeholder\"
-")
-
-# Append command to copy the WebUI files if required
-if(DEFINED UI_SOURCE_PATH)
-    set(copy_command "${copy_command} &&
-        ${CMAKE_COMMAND} -E copy_directory \"${_web_ui_build_folder_path}\" \"${_full_fs_dest_image_path}\"
-    ")
-endif()
-
-# Replace newlines and multiple spaces (for readability) with a single space
-string(REGEX REPLACE "\n" " " copy_command "${copy_command}")
-string(REGEX REPLACE "[ ]+" " " copy_command "${copy_command}")
-
 # Define a custom target to always copy FS and WebUI files
 add_custom_target(CopyFSAndWebUI ALL
-    COMMAND ${CMAKE_COMMAND} -E echo "Running CopyFSAndWebUI target" &&
-            ${copy_command}
+    COMMAND ${CMAKE_COMMAND} -E echo "------------------ Running CopyFSAndWebUI target --------------------"
+    COMMAND ${CMAKE_COMMAND} -E remove_directory "${_full_fs_dest_image_path}"
+    COMMAND ${CMAKE_COMMAND} -E copy_directory "${_full_fs_source_image_path}" "${_full_fs_dest_image_path}"
+    COMMAND ${CMAKE_COMMAND} -E remove "${_full_fs_dest_image_path}/placeholder"
+    # Only execute the next command if UI_SOURCE_PATH is defined
+    $<$<BOOL:${UI_SOURCE_PATH}>:${CMAKE_COMMAND} -E copy_directory "${_web_ui_build_folder_path}" "${_full_fs_dest_image_path}">
     DEPENDS WebUI AlwaysCopyFSAndWebUI # Ensure WebUI target is built first
     COMMENT "Copying FS and WebUI files to the FS Image directory"
 )
