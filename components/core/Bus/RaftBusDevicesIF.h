7804feb902399d11a432b6ecb8a723632d3a930e
Rob Dobson <rob@dobson.com>
2024-05-31 12:57:02 -0400

    Updated decodePollResponses

--- components/core/Bus/RaftBusDevicesIF.h
+++ components/core/Bus/RaftBusDevicesIF.h
@@ -37,8 +37,8 @@ public:
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// @brief Get device type information by address
-    /// @param address - address of device to get information for
-    /// @param includePlugAndPlayInfo - true to include plug and play information
+    /// @param address address of device to get information for
+    /// @param includePlugAndPlayInfo true to include plug and play information
     /// @return JSON string
     virtual String getDevTypeInfoJsonByAddr(uint32_t address, bool includePlugAndPlayInfo) const = 0;
 
@@ -56,15 +56,15 @@ public:
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// @brief Get decoded poll responses
-    /// @param deviceTypeIndex index of device type
-    /// @param pPollBuf buffer containing poll responses
-    /// @param pollBufLen length of poll response buffer
+    /// @param address address of device to get data from
     /// @param pStructOut pointer to structure (or array of structures) to receive decoded data
     /// @param structOutSize size of structure (in bytes) to receive decoded data
     /// @param maxRecCount maximum number of records to decode
+    /// @param decodeState decode state for this device
     /// @return number of records decoded
-    virtual uint32_t decodePollResponses(uint16_t deviceTypeIndex, 
-                    const uint8_t* pPollBuf, uint32_t pollBufLen, 
+    /// @note the pStructOut should generally point to structures of the correct type for the device data and the
+    ///       decodeState should be maintained between calls for the same device
+    virtual uint32_t decodePollResponses(uint32_t address, 
                     void* pStructOut, uint32_t structOutSize, 
                     uint16_t maxRecCount, BusDeviceDecodeState& decodeState) = 0;
 };
