36646dd9507061e16672064a0f54a63bc75bb987
Rob Dobson <rob@dobson.com>
2024-06-04 19:33:46 +0100

    Trim WiFi SSID

--- components/core/NetworkSystem/NetworkSystem.cpp
+++ components/core/NetworkSystem/NetworkSystem.cpp
@@ -422,6 +422,7 @@ bool NetworkSystem::startWifi()
 
             // Set SSID we're trying to connect to
             String ssid = String((char*)currentWifiConfig.sta.ssid, sizeof(currentWifiConfig.sta.ssid));
+            ssid.trim();
             _wifiStaSSIDConnectingTo = ssid;
 
             // Debug
@@ -862,6 +863,7 @@ void NetworkSystem::wifiEventHandler(void *pArg, int32_t eventId, void *pEventDa
         _wifiStaSSID = String((const char*)(pEvent->ssid),
                     pEvent->ssid_len > sizeof(wifi_event_sta_connected_t::ssid) ?
                     sizeof(wifi_event_sta_connected_t::ssid) : pEvent->ssid_len);
+        _wifiStaSSID.trim();
         xEventGroupSetBits(_networkRTOSEventGroup, WIFI_STA_CONNECTED_BIT);
         LOG_NETWORK_EVENT_INFO(MODULE_PREFIX, "WiFi station connected");
         break;
