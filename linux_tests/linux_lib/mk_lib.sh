7e36319c3edaf7693068368790a07ac7d86ba17f
Rob Dobson <robdobsn@users.noreply.github.com>
2024-12-17 00:57:17 +0000

    Reduced linux build verbosity

--- linux_tests/linux_lib/mk_lib.sh
+++ linux_tests/linux_lib/mk_lib.sh
@@ -2,6 +2,7 @@ rm -rf build_linux/
 mkdir build_linux
 cd build_linux
 cmake -DCMAKE_INSTALL_PREFIX=install ..
-make -j$(nproc) VERBOSE=1
+make -j$(nproc)
+# make -j$(nproc) VERBOSE=1
 make install
 cd ..
