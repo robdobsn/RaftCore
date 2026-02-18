5b3c8c9a93ddc781517d47adfb727ae4370f3f63
Rob Dobson <robdobsn@users.noreply.github.com>
2026-02-18 17:28:37 +0000

    Added support for local library files for development
    Development libraries can be placed in ./libs
    E.g. cloning RaftCore to the ./libs folder allows for local development and git support
    Also improved handling of missing WebUI folder / package.json

--- scripts/RaftBootstrap.cmake
+++ scripts/RaftBootstrap.cmake
@@ -100,20 +100,47 @@ foreach(_raft_component ${RAFT_COMPONENTS})
     # Create lowercase version for FetchContent variable names
     string(TOLOWER ${_raft_component_name} _raft_component_lower)
 
-    # Fetch the Raft library
-    FetchContent_Declare(
-        ${_raft_component_lower}
-        SOURCE_DIR     ${RAFT_BUILD_ARTIFACTS_FOLDER}/${_raft_component_name}
-        GIT_REPOSITORY ${_raft_repo_url}
-        GIT_TAG        ${_raft_component_tag}
-    )
-    FetchContent_Populate(${_raft_component_lower})
+    # Check if a local copy of this library exists in ./libs/<ComponentName>
+    set(_local_lib_path "${CMAKE_SOURCE_DIR}/libs/${_raft_component_name}")
+    if(EXISTS "${_local_lib_path}" AND IS_DIRECTORY "${_local_lib_path}")
 
-    # Add the component dir to the list of extra component dirs
-    set(EXTRA_COMPONENT_DIRS ${EXTRA_COMPONENT_DIRS} ${${_raft_component_lower}_SOURCE_DIR})
+        # Use the local library instead of fetching from remote
+        message(STATUS "")
+        message(STATUS "============================================================")
+        message(STATUS "  LOCAL (DEBUG) LIBRARY: ${_raft_component_name}")
+        message(STATUS "  Using: ${_local_lib_path}")
+        message(STATUS "  (Skipping fetch from ${_raft_repo_url})")
+        message(STATUS "============================================================")
+        message(STATUS "")
 
-    # Add the component to the list of dependencies
-    set(ADDED_PROJECT_DEPENDENCIES ${ADDED_PROJECT_DEPENDENCIES} ${_raft_component_lower})
+        # Set the _SOURCE_DIR variable that FetchContent_Populate would normally set
+        # (downstream scripts like RaftBootstrapPhase2.cmake reference ${raftcore_SOURCE_DIR} etc.)
+        set(${_raft_component_lower}_SOURCE_DIR "${_local_lib_path}" CACHE PATH "" FORCE)
+
+        # Add the local library dir to extra component dirs
+        set(EXTRA_COMPONENT_DIRS ${EXTRA_COMPONENT_DIRS} ${_local_lib_path})
+
+        # Add the component to the list of dependencies
+        set(ADDED_PROJECT_DEPENDENCIES ${ADDED_PROJECT_DEPENDENCIES} ${_raft_component_lower})
+
+    else()
+
+        # Fetch the Raft library from remote
+        FetchContent_Declare(
+            ${_raft_component_lower}
+            SOURCE_DIR     ${RAFT_BUILD_ARTIFACTS_FOLDER}/${_raft_component_name}
+            GIT_REPOSITORY ${_raft_repo_url}
+            GIT_TAG        ${_raft_component_tag}
+        )
+        FetchContent_Populate(${_raft_component_lower})
+
+        # Add the component dir to the list of extra component dirs
+        set(EXTRA_COMPONENT_DIRS ${EXTRA_COMPONENT_DIRS} ${${_raft_component_lower}_SOURCE_DIR})
+
+        # Add the component to the list of dependencies
+        set(ADDED_PROJECT_DEPENDENCIES ${ADDED_PROJECT_DEPENDENCIES} ${_raft_component_lower})
+
+    endif()
 
 endforeach()
 
