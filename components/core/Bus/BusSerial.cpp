cc2ada7e8a63d746c950973a9e5eb642cfe1adfd
Rob Dobson <robdobsn@users.noreply.github.com>
2026-09-21 14:32:04 +0100

    Fix required for ESP IDF 6.1

--- components/core/Bus/BusSerial.cpp
+++ components/core/Bus/BusSerial.cpp
@@ -122,6 +122,9 @@ bool BusSerial::serialInit()
             .stop_bits = UART_STOP_BITS_1,
             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
             .rx_flow_ctrl_thresh = 10,
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 1, 0)
+            .rx_glitch_filt_thresh = 0,
+#endif
 #if ESP_IDF_VERSION <= ESP_IDF_VERSION_VAL(5, 0, 0)
                 .use_ref_tick = false,
 #else
